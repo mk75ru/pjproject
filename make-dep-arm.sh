552442d88037b0f39b76b5919d308de9705dec65
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-08-14 12:35:17 +0700

    добавил скриптов для сборки

--- make-dep-arm.sh
+++ make-dep-arm.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-get_rv1126; make -j 7 dep
+alias get_rv1126_='export PATH="/sdk/rv1126_rv1109_linux_release_20211022/buildroot/output/firefly_rv1126_rv1109/host/bin":"/usr/lib/python3.11/site-packages/":$PATH; export LD_LIBRARY_PATH="/sdk/rv1126_rv1109_linux_release_20211022/buildroot/output/firefly_rv1126_rv1109/host/arm-buildroot-linux-gnueabihf/sysroot/usr/lib":$LD_LIBRARY_PATH'
+
+get_rv1126_; make -j 7 dep
