8e7792f635a808195aec63692f825ebb38d1de18
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-09-18 16:14:52 +0700

    добавил запуск в фоне в pjsua-example.sh

--- pjsua-example.sh
+++ pjsua-example.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-pjsua  --cli-telnet-port=9090 --use-cli --realm=asterisk  --id=sip:101@127.0.0.1  --registrar=sip:127.0.0.1 --no-tcp --username=101 --password=101  --auto-answer=200 --ec-tail=30 --ec-opt=4
+#pjsua  --cli-telnet-port=9090 --use-cli --realm=asterisk  --id=sip:101@127.0.0.1  --registrar=sip:127.0.0.1 --no-tcp --username=101 --password=101  --auto-answer=200 --ec-tail=30 --ec-opt=4
+setsid pjsua  --use-cli --no-cli-console --realm=asterisk  --id=sip:101@127.0.0.1  --registrar=sip:127.0.0.1 --no-tcp --username=101 --password=101  --auto-answer=200 --ec-tail=30 --ec-opt=4 &
