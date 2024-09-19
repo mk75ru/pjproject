97b6c77fe304928741e23c432cca746ab43b0535
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-09-19 11:40:39 +0700

    отключил opus

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -1,6 +1,6 @@
 //#define PJ_HAS_SSL_SOCK 1
 #define PJMEDIA_HAS_VIDEO    1
-#define  PJMEDIA_HAS_OPUS_CODEC 1
+//#define  PJMEDIA_HAS_OPUS_CODEC 1
 //#define PJMEDIA_VIDEO_DEV_HAS_SDL 1
 //#define PJMEDIA_VIDEO_DEV_SDL_HAS_OPENGL    1
 #include <pj/config_site_sample.h>
