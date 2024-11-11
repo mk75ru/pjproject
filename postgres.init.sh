8e414cbec1c9e8d59a8d8259ea71918d3d8a366d
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-11-11 10:40:18 +0700

    добавил проверку существования базы данных

--- postgres.init.sh
+++ postgres.init.sh
@@ -3,11 +3,9 @@
 source ./.env
 
 export PGPASSWORD="incom"
+FORCE=$1
 
-if psql -lqt | cut -d \| -f 1 | grep -qw ${DB_NAME}; then
-    echo "база ${DB_NAME} уже существует"
-else
-    echo "база ${DB_NAME} не существует, создаем"
+createdb() {
     psql -U postgres -c "DROP DATABASE IF EXISTS ${DB_NAME};"
     psql -U postgres -c "DROP ROLE IF EXISTS ${DB_USER};"
     psql -U postgres -c "CREATE DATABASE ${DB_NAME};"
@@ -16,4 +14,17 @@ else
     psql -U postgres --dbname=${DB_NAME} -c "GRANT ALL PRIVILEGES ON SCHEMA public TO ${DB_USER};"
     psql -U postgres --username=${DB_USER} --dbname=${DB_NAME} --host=localhost -c "CREATE SCHEMA ${DB_SCHEMA};"
     psql -U postgres --username=${DB_USER} --dbname=${DB_NAME} --host=localhost -c "CREATE TABLE ${DB_SCHEMA}.${DB_TABLE}(${DB_TABLE_COLUMNS});"
+}
+
+
+if psql -lqt | cut -d \| -f 1 | grep -qw ${DB_NAME}; then
+    echo "база ${DB_NAME} уже существует"
+    if [[ ${FORCE} == "-f" ]]
+    then
+        echo "принудительно пересоздаем базу ${DB_NAME}"
+        createdb
+    fi
+else
+    echo "база ${DB_NAME} не существует, создаем"
+    createdb
 fi
