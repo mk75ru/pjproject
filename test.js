622a211167a7fb92db03f45e84028382e8f36eb1
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-11-13 14:13:52 +0700

    переделал формат отправки данных события в журнале

--- test.js
+++ test.js
@@ -11,25 +11,34 @@ import {logger,po} from './logger.js';
       logger.info('REQUEST ---------------------- 1 ---------------------');
       let request ={startDate: 1731380232, endDate: 1731380232*2, evType:["alarmStart","alarmEnd"]}
       let request_j = JSON.stringify(request)
-      let rc =  await  evjrnl.get(request_j);
+      let rc =  await  evjrnl.get(request);
+      let  payload =  {err: "success", data: rc}
+      logger.info('REQUEST result:: %s ', po(payload));
+
     }
     {
       logger.info('REQUEST ---------------------- 2 ---------------------');
       let request ={startDate: 1731380232, endDate: 1731380232*2, evType:[]}
       let request_j = JSON.stringify(request)
-      let rc =  await  evjrnl.get(request_j);
+      let rc =  await  evjrnl.get(request);
+      let  payload =  {err: "success", data: rc}
+      logger.info('REQUEST result:: %s ', po(payload));
     }
     {
       logger.info('REQUEST ---------------------- 3 ---------------------');
       let request ={idSess:1, evType:["alarmStart"]}
       let request_j = JSON.stringify(request)
-      let rc =  await  evjrnl.get(request_j);
+      let rc =  await  evjrnl.get(request);
+      let  payload =  {err: "success", data: rc}
+      logger.info('REQUEST result:: %s ', po(payload));
     }
     {
       logger.info('REQUEST ---------------------- 4 ---------------------');
       let request ={startDate: 1731380232, endDate: 1731380232*2}
       let request_j = JSON.stringify(request)
-      let rc =  await  evjrnl.get(request_j);
+      let rc =  await  evjrnl.get(request);
+      let  payload =  {err: "success", data: rc}
+      logger.info('REQUEST result:: %s ',po(payload));
     }
   }
   catch(e) {
