43bbff1df87b89f4759fbd2fb1c5c40fedf736a4
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-11-13 14:54:22 +0700

    доработка по логированию

--- eventsJournal.js
+++ eventsJournal.js
@@ -61,7 +61,9 @@ export default class eventsJournal {
     try {
       const result = await pool.query('INSERT INTO events_schema.events_table  (date_ev ,name_ev, description_ev,id_session_ev ,version_data_ev, data_ev) VALUES (TO_TIMESTAMP($1), $2, $3, $4, $5, $6) RETURNING *',
                                       [date_ev_sec ,name_ev, description_ev,this._idSess ,version_data_ev, data_ev]);
-      logger.info('eventsJournal: insert: %s', po(result.rows));
+      logger.info('eventsJournal: date_ev_sec: %s name_ev=%s description_ev=%s idSess=%s',
+                  date_ev_sec ,name_ev, description_ev, this._idSess);
+      logger.trace('eventsJournal: insert: %s', po(result.rows));
       if( name_ev === "alarmEnd") {
         this._idSess  = 0;
       }
@@ -82,14 +84,15 @@ export default class eventsJournal {
     try {
       let unixtime_sec = Math.round(new Date().getTime() / 1000)
       const result_ts = await pool.query('SELECT TO_TIMESTAMP($1)', [unixtime_sec]);
-      logger.info('eventsJournal: ts:%s', po(result_ts.rows[0].to_timestamp));
+      logger.trace('eventsJournal: ts:%s', po(result_ts.rows[0].to_timestamp));
       let date_ev =  unixtime_sec
       let human_date_ev =  result_ts.rows[0].to_timestamp;
-      logger.info('eventsJournal: date_ev:%s',date_ev);
-      logger.info('eventsJournal: human_date_ev:%s',po(human_date_ev));
       const result = await pool.query('INSERT INTO events_schema.events_table  (date_ev , human_date_ev ,name_ev, description_ev,id_session_ev ,version_data_ev, data_ev) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
                                       [date_ev,human_date_ev,name_ev, description_ev,this._idSess ,version_data_ev, data_ev]);
-      logger.info('eventsJournal: insert: %s', po(result.rows));
+      logger.info('eventsJournal: date_ev=%s human_date_ev=%s  name_ev=%s description_ev=%s idSess=%s',
+                  date_ev , human_date_ev ,name_ev, description_ev,this._idSess);
+      logger.trace('eventsJournal: insert: %s', po(result.rows));
+
       if( name_ev === "alarmEnd") {
         this._idSess  = 0;
       }
@@ -110,7 +113,7 @@ export default class eventsJournal {
     try {
       const result = await pool.query('UPDATE events_schema.events_table SET  human_data_ev = $1  WHERE id = $2 RETURNING *',
                                       [ human_data_ev, id]);
-      logger.info('eventsJournal: update: %s ',  po(result.rows));
+      logger.trace('eventsJournal: update: %s ',  po(result.rows));
     } catch (err) {
       logger.error(err.stack,"eventsJournal: update");
     }
@@ -122,7 +125,7 @@ export default class eventsJournal {
     }
     try {
       const result =await pool.query('DELETE FROM events_schema.events_table WHERE id = $1', [id]);
-      logger.info('eventsJournal: delete: %s ',  po(result.rows));
+      logger.trace('eventsJournal: delete: %s ',  po(result.rows));
     } catch (err) {
       logger.error(err.stack,"eventsJournal: delete");
     }
@@ -172,7 +175,8 @@ export default class eventsJournal {
                                   [req.idSess ]);
         }
       }
-      logger.info('eventsJournal: get: %s ',  po(result.rows));
+      logger.info('eventsJournal: get length: %s ', result.rows.length );
+      logger.trace('eventsJournal: get: %s ',  po(result.rows));
       return  result.rows;
     } catch (err) {
       logger.error(err.stack,"eventsJournal: get");
@@ -183,7 +187,7 @@ export default class eventsJournal {
   async getAll() {
     try {
       const result = await pool.query('SELECT * FROM events_schema.events_table');
-      logger.info('eventsJournal: get all: %s ',  po(result.rows));
+      logger.trace('eventsJournal: get all: %s ',  po(result.rows));
     } catch (err) {
       logger.error(err.stack,"eventsJournal: get all");
     }
