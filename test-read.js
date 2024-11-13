6658deb3ea5a55b72d2f81b04067be09e25579c8
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2024-11-13 15:05:58 +0700

    доработка по тестам

--- test-read.js
+++ test-read.js
@@ -5,8 +5,8 @@ import {logger,po} from './logger.js';
     const evj  = await import('eventsJournal');
     let evjrnl = new evj.default;
     await evjrnl.run();
-    await evjrnl.insert("alarmStart", "Начало оповещения",1, {idSess:1})
-    await evjrnl.insert("alarmEnd", "Окончание оповещения",1, {idSess:1})
+//    await evjrnl.insert("alarmStart", "Начало оповещения",1, {idSess:1})
+//    await evjrnl.insert("alarmEnd", "Окончание оповещения",1, {idSess:1})
     {
       logger.info('REQUEST ---------------------- 1 ---------------------');
       let request ={startDate: 1731380232, endDate: 1731380232*2, evType:["alarmStart","alarmEnd","connected"]}
